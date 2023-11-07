2abd556d90b963dfd0e181d25159ac84fb71d6f7
Faisal A <akhtarfaisal147@gmail.com>
2023-11-07 15:54:16 +0100

    add LICENSE and to all source files

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,25 @@
-// Copyright© Faisal A.
+/*******************************************************************************
+MIT License
 
-// POPUP [ popup.js ]
-// Script for the extension's popup-page.
+Copyright (c) 2023 Faisal A.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the “Software”), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
 
 const body = document.querySelector('body');
 const btnChangeWidth = document.querySelector('.btnChangeWidth');
@@ -12,11 +30,10 @@ let isShortened = false;
 
 // Add listener to the button
 btnChangeWidth.addEventListener('click', async () => {
-    
     let queryOptions = { active: true, currentWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
-    
-    chrome.scripting.executeScript({ 
+
+    chrome.scripting.executeScript({
         target: { tabId: tab.id } ,
         args: [inp.value],
         func: changeWidthOfBody
@@ -25,7 +42,7 @@ btnChangeWidth.addEventListener('click', async () => {
 
 /**
  * Change the width of the body and center it.
- * 
+ *
  * @param {Number} toNewWidth The new width in pixels. Can be null.
  */
 function changeWidthOfBody(toNewWidth) {
